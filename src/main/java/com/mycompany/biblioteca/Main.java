a4ac3516497a7e7a476d18a08735787075970c15
GustavoRodriguez <gustavoandresrodriguez@unicesar.edu.co>
2026-08-18 23:04:44 -0500

    feat: implementar CREATE de Cliente

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -1,16 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.biblioteca;
 
-/**
- *
- * @author Desktop
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
+    static ArrayList<Cliente> clientes = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Aquí irá el menú (Fase 8)
+    }
+
+    static void crearCliente() {
+        System.out.print("Ingrese ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Ingrese nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Ingrese teléfono: ");
+        String telefono = sc.nextLine();
+
+        Cliente cliente = new Cliente(id, nombre, telefono);
+        clientes.add(cliente);
+
+        System.out.println("Cliente creado correctamente.");
     }
 }
